c8ffe94a099a8274f8384577d37801b6f3a4b4bf
mrtambour <6724649+mrtambour@users.noreply.github.com>
2019-03-29 21:03:22 -0700

    removed redundant code, added the regex filter,

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
 extern crate regex;
+extern crate reqwest;
 
-use std::fs::File;
-use std::io::BufReader;
-use std::io::prelude::*;
+use select::document::Document;
+use select::predicate::Name;
+use regex::Regex;
+use std::io::Read;
 use std::net::TcpStream;
 use twitchchat::commands::PrivMsg;
 use twitchchat::{Client, Writer, UserConfig, TWITCH_IRC_ADDRESS};
@@ -14,13 +16,15 @@ fn main() {
         .expect("must be able to clone the tcp stream");
 
     let config = UserConfig::builder()
-        .token("OAUTH_TOKEN")
-        .nick("BOT_USERNAME")
+        .token("OAUTH")
+        .nick("USERNAME")
         .membership()
         .commands()
         .tags()
         .build()
         .expect("partial configuration initialized");
+
+    //let CHANNEL = "CHANNEL";
 
     let mut client = Client::new(read, write);
 
@@ -34,7 +38,7 @@ fn main() {
         user.color.unwrap_or_default()
     );
 
-    client.on(|msg: PrivMsg, _: Writer<_>| {
+    client.on(|msg: PrivMsg, wr: Writer<_>| {
         let name = msg.display_name().unwrap_or_else(|| msg.user());
         use twitchchat::BadgeKind::{Broadcaster, Subscriber, Moderator};
 
@@ -50,42 +54,49 @@ fn main() {
             badges.contains(&Broadcaster),
             badges.contains(&Subscriber),
             msg.moderator(),
-            ) {
+        ) {
             (true, _, _) => println!("{} --> SAFE", name),
             (_, true, _) => println!("{} --> SAFE", name),
             (_, _, true) => println!("{} --> SAFE", name),
             (_, _, _) => {
-                println!("{} !!! Beginning Analysis");
+                println!("{} !!! Beginning Analysis", msg.message);
+                let msg_text = msg.message;
 
-                if msg.message.contains("KEYS_FILE") {
-                    println!("Filtering");
-                    let file = File::open("PATH_TO_FILE").expect("error opening file");
-                    let mut read_buffer = BufRead::new(file);
-                    let mut contents = String::new();
-                    buf_reader.read_to_string(&mut contents);
-                    // to be completed
-                    // scan buffer
-                    // make it optional
-
-                } else if msg.message.contains("youtube.com" | "youtu.be.com") {
+                if msg_text.contains("youtube.com") {
                     println!("Youtube Link Detected");
-                    // to be completed
-                    // request html page
-                    // parse html page
-                    // respond
-                    // gather info
+                    println!("message text: {}", &msg_text);
+                    let regexpression = Regex::new(r"\?v=([a-zA-Z0-9-]+)").unwrap();
+                    if let Some(cap) = regexpression.captures_iter(&msg_text).next() {
+                        println!("beginning capture: {:?}", cap);
+                        let video_id = cap[0].to_string();
+                        let video_id_copy = cap[1].to_string();
+                        let complete_url = format!("https://www.youtube.com/watch?v={}", video_id);
+                        let mut response = reqwest::get(&complete_url).expect("error getting page");
+                        let mut buffer = String::new();
+                        response.read_to_string(&mut buffer);
+                        println!("buffer: {:?}", buffer);
 
-                } else if msg.message.contains("www." | "http" | "://" | "goo.gl") {
-                    println!("Link Detected");
-                    // to be completed
-                    // request html page
-                    // parse html page
-                    // respond
-                    // make it optional
+                        // thread index out of bounds: the len is 0 but the index is 0' panic after this point
+                        let html: &str = &buffer;
+                        let document = Document::from(html);
+                        let view_count = document.find(Name("a"))
+                                                 .filter_map(|n| n.attr(""));
+                        let mut text_bucket: Vec<&str> = vec![];
+                        for text in view_count {
+                            text_bucket.push(text);
+                            println!("{}", text);
+                        }
+                        // panic is right here for text bucket
+                        println!("bucket: {}", text_bucket[0]);
 
-                } else {
-                    println!("OK");
-                    // to be completed
+                        let final_message = format!("Video Views: {:?}", text_bucket.pop().expect("error popping vector"));
+                        //println!("views: {:?}", text_bucket[0]);
+                        wr.send("CHANNEL", final_message).unwrap();
+                    }
+
+                } else if msg_text.contains("!sr") {
+                    println!("Analyzing abnormal request");
+                    // scan as link or search
                 }
             }
         }
@@ -94,8 +105,7 @@ fn main() {
 
     let w = client.writer();
     w.join("CHANNEL").unwrap();
-    w.send("CHANNEL", "I have arrived!").unwrap();
-
+    w.send("USERNAME", "I have arrived!").unwrap();
     client.run();
 
 }
